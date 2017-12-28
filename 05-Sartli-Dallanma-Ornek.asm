eae311b91a7ea3a84c2452cdb5c7c7426f751b0c
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-12-29 01:16:52 +0300

    Update 05-Sartli-Dallanma-Ornek.asm

--- 05-Sartli-Dallanma-Ornek.asm
+++ 05-Sartli-Dallanma-Ornek.asm
@@ -17,10 +17,20 @@ start:
     dongu:
         sub ax,bx
         dec ax      ; ax'i 1 azalt
-        inc bx      ; bx'i 1 artýr
+        inc bx      ; bx'i 1 artÃ½r
         cmp ax,5    ; ax ile 5'i karsilastir
         ja dongu    ; ax 5'ten buyukse donguye'git (jump if above)
      
 ends
-
 end start
+
+;ja    = jump if above
+;jb    = jump if below
+;je    = jump if equal
+;jae   = jump if above or equal
+;jbe   = jump if below or equal
+;jna   = jump if not above
+;jnb   = jump if not below     
+;jne   = jump if not equal
+;jnae  = jump if not above or equal
+;jnbe  = jump if not below or equal
